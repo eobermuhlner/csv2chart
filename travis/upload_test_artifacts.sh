6b5606fd179cbb0c7d84fb840a0e5759dbf501df
Eric Obermühlner <eobermuhlner@gmail.com>
2017-09-01 16:46:56 +0200

    uploading of travis artifacts does not work yet

--- travis/upload_test_artifacts.sh
+++ travis/upload_test_artifacts.sh
@@ -7,6 +7,7 @@ mkdir experiments/csv2chart
 cp ./ch.obermuhlner.csv2chart/src/test/resources/out_images/*.png experiments/csv2chart/
 
 cd experiments
-git add csv2char/*
-git commit -m "Travis"
-echo "$UPLOAD_ARTIFACT_PASSWORD" | git push --repo "https://$UPLOAD_ARTIFACT_USERNAME:github.com/eobermuhlner/experiments.git"
+git add csv2chart/*
+git commit -m "Travis `date`"
+git push --repo "https://$UPLOAD_ARTIFACT_USERNAME:$UPLOAD_ARTIFACT_PASSWORD@github.com/eobermuhlner/experiments.git"
+cd ..
